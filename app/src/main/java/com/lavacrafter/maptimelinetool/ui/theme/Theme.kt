0cc0413b4bd8a5ca004cb1ed46bd3d8645ac47f1
Lava_crafter <183024981+muchenjiang@users.noreply.github.com>
2026-01-16 21:14:29 -0400

    Add Compose theme

---
+++ app/src/main/java/com/lavacrafter/maptimelinetool/ui/theme/Theme.kt
@@ -0,0 +1,16 @@
+package com.lavacrafter.maptimelinetool.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColors = lightColorScheme()
+
+@Composable
+fun MapTimelineToolTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = LightColors,
+        typography = androidx.compose.material3.Typography(),
+        content = content
+    )
+}
