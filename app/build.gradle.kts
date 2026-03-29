0ac4e1fcef161ab6e00a4c916f2fb451a0e4cc3c
muchenjiang <183024981+muchenjiang@users.noreply.github.com>
2026-03-28 21:27:10 -0300

    version code 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -148,7 +148,7 @@ android {
         applicationId = "com.lavacrafter.maptimelinetool"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
+        versionCode = 8
         versionName = "0.1.6"
     }
 
